af11a23d7c3fbc6c726b89896a0338ab639090da
JOSE LUIS RODRIGUEZ CANO <jlrodriguez@krugerschool.edu.ec>
2024-06-19 20:30:32 -0500

    update

--- api/users/routes.js
+++ api/users/routes.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const controller = require('./controller')
 const passport = require('passport');
 
-router.patch('/', passport.authenticate('jwt',{session:false}) ,controller.update);
+router.patch('/update-profile/:id', passport.authenticate('jwt',{session:false}) ,controller.updateUserById);
+router.patch('/update-profile', passport.authenticate('jwt',{session:false}) ,controller.updateCurrentUser);
 router.get('/', passport.authenticate('jwt',{session:false}) ,controller.getAllUsers);
+router.get('/:id', passport.authenticate('jwt', { session: false }), controller.getUserById);
 
 module.exports = router
