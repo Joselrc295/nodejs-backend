bafec7ee1df9813f7545445503a8f7c357a60007
Edu_Davila <edavila@krugerschool.edu.ec>
2024-06-28 17:57:21 -0500

    add view flat

--- api/flats/routes.js
+++ api/flats/routes.js
@@ -7,6 +7,7 @@ const authController = require('../auth/controller')
 router.post('/' , passport.authenticate('jwt',{session:false}) ,authController.isLandlord , controller.createFlats);
 router.get('/' , passport.authenticate('jwt',{session:false}) ,authController.isLandlord , controller.getAllFlats);
 router.get('/my' , passport.authenticate('jwt',{session:false}) , controller.getMyFlats);
+router.get('/:id',  passport.authenticate('jwt',{session:false}) , controller.getFlatByID)
 
 
 module.exports = router
