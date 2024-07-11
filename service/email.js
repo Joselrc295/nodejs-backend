f011cb2f13fa24c56a1df0feff93400a8902e540
JOSE LUIS RODRIGUEZ CANO <jlrodriguez@krugerschool.edu.ec>
2024-07-10 20:16:16 -0500

    mejor email

--- service/email.js
+++ service/email.js
@@ -1,27 +1,26 @@
 const nodemailer = require('nodemailer');
 const config = require('../settings/config');
 
-const sendEmail = async options => {
+const sendEmail = async (options) => {
     let transporter = nodemailer.createTransport({
-        host: config.EMAIL_HOST,
-        port: config.EMAIL_PORT,
-        auth: {
-            user: config.EMAIL_USER,
-            pass: config.EMAIL_PASSWORD
-        },
-        tls: {
-            rejectUnauthorized: false
-        }
-    })
+      host: config.EMAIL_HOST,
+      port: config.EMAIL_PORT,
+      auth: {
+        user: config.EMAIL_USER,
+        pass: config.EMAIL_PASSWORD,
+      },
+      tls: {
+        rejectUnauthorized: false,
+      },
+    });
+  
     const mailOptions = {
-        from: '<test mail> no-reply',       
-        to: options.email,
-        subject: options.subject,
-        text: options.message
-    }
-
-   return  await transporter.sendMail(mailOptions)
-
-}
-
+      from: '<test mail> no-reply',       
+      to: options.email,
+      subject: options.subject,
+      html: options.message, // Usa la propiedad html en lugar de text para contenido HTML
+    };
+  
+    return await transporter.sendMail(mailOptions);
+  };
 module.exports = sendEmail;
